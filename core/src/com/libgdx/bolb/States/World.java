e31a01a12770d53ca6d8dc0bef6f3057e03bf234
Vibhor Singh <vibdaddy12@gmail.com>
2020-02-09 17:52:34 +0100

    made world

--- core/src/com/libgdx/bolb/States/World.java
+++ core/src/com/libgdx/bolb/States/World.java
@@ -1,4 +1,41 @@
 package com.libgdx.bolb.States;
 
-public class World {
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.libgdx.bolb.Management.GameStateManagement.GameStateManager;
+import com.libgdx.bolb.Management.GameStateManagement.State;
+
+public class World extends State {
+
+
+
+    public World(GameStateManager gsm){
+        super(gsm);
+
+
+
+
+
+
+    }
+
+
+    @Override
+    protected void HandleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
