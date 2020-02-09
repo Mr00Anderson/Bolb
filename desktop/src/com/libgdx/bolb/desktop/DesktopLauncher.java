120d8026ace3451db5e478712c55d91a00c913a6
Vibhor Singh <vibdaddy12@gmail.com>
2020-02-09 15:32:46 +0100

    Upload

---
+++ desktop/src/com/libgdx/bolb/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.libgdx.bolb.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.libgdx.bolb.Bolb;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Bolb(), config);
+	}
+}
