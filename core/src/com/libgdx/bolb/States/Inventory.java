7003a7804ee2c8d75d95823ef98e8c66f5d21940
Vibhor Singh <vibdaddy12@gmail.com>
2020-02-10 15:14:16 +0100

    added inventory state

---
+++ core/src/com/libgdx/bolb/States/Inventory.java
@@ -0,0 +1,34 @@
+package com.libgdx.bolb.States;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.libgdx.bolb.Management.GameStateManagement.GameStateManager;
+import com.libgdx.bolb.Management.GameStateManagement.State;
+
+public class Inventory extends State {
+
+    public Inventory(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    protected void HandleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+
+}
