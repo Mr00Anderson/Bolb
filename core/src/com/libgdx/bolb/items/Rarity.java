989380d0b331f0c13f9ad84acbd3bc4e2f9d5f47
Sashie <ud13n0w@gmail.com>
2020-02-24 13:24:09 -0330

    add level to rarity

--- core/src/com/libgdx/bolb/items/Rarity.java
+++ core/src/com/libgdx/bolb/items/Rarity.java
@@ -2,13 +2,23 @@ package com.libgdx.bolb.items;
 
 public enum Rarity {
 
-	COMMON,
-	UNCOMMON,
-	RARE,
-	EPIC,
-	LEGENDARY,
-	MYTHICAL,
-	FORBIDDEN,
-	TITAN;
+	COMMON(1),
+	UNCOMMON(2),
+	RARE(3),
+	EPIC(4),
+	LEGENDARY(5),
+	MYTHICAL(6),
+	FORBIDDEN(7),
+	TITAN(8);
+	
+	int level;
+	
+	private Rarity(int level) {
+		this.level = level;
+	}
+	
+	public int getLevel() {
+		return this.level;
+	}
 	
 }
